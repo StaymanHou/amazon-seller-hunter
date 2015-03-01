e0182d36b51f5419c99329b39744547dc8d58c61
Stayman Hou <hhyycc0418@gmail.com>
2015-03-01 13:06:17 -0800

    fix bug for two rules

--- src/lib/rules/positive_must_larger_than_rule.rb
+++ src/lib/rules/positive_must_larger_than_rule.rb
@@ -1,5 +1,5 @@
 class PositiveMustLargerThanRule < RuleProto
   def filter(seller)
-    seller.rate <= @settings['larger_than']
+    seller.rate <= @settings['larger_than'].to_f
   end
 end
--- src/lib/rules/total_ratings_must_larger_than_rule.rb
+++ src/lib/rules/total_ratings_must_larger_than_rule.rb
@@ -1,5 +1,5 @@
 class TotalRatingsMustLargerThanRule < RuleProto
   def filter(seller)
-    seller.total_ratings <= @settings['larger_than']
+    seller.total_ratings <= @settings['larger_than'].to_f
   end
 end
